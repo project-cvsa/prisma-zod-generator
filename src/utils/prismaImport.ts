c30180dbcc4ced020d5089b0abb9c661dc726dc6
alikia2x <alikia2x@outlook.com>
2026-03-22 16:55:50 +0800

    refactor: remove all the files

--- src/utils/prismaImport.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import Transformer from '../transformer';
-
-export function getPrismaImportSpecifier(targetDir: string): string {
-  const resolvedDir = path.resolve(targetDir);
-  return Transformer.resolvePrismaImportPath(resolvedDir);
-}
